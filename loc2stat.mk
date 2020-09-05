19242f08e5cd11d810b69ba30f21d26b391945bf
Thierry Delamare <t.delamare@laposte.net>
2020-09-05 21:56:34 +0200

    Works on loc2stat.mk

--- loc2stat.mk
+++ loc2stat.mk
@@ -18,53 +18,98 @@ $(self):;
 
 € = $(subst €,$$,$1)
 
-columns := meta dev mount inode nlinks mode atime mtine ctime uname gname uid gid size path
-pathsep := :
+columns.name := meta dev mount inode nlinks mode atime mtime ctime uname gname uid gid size path
+columns.type := dec  hex str   dec   dec    hex  time  time  time  str   str   dec dec dec  path
+path.sep := :
+
+columns.use := meta mount inode nlinks atime mtime ctime uname gname uid gid path
+columns.use := size mount
 
 ~ := cols
 ifeq ($(MAKECMDGOALS),$~)
-$~: names = $(columns)
+$~: names = $(columns.name)
 $~: cnt = $(words $(names))
-$~: $~ = (seq $(cnt) | xargs; echo $(names)) | column -t
+$~: types = $(columns.type)
+$~: $~ = (seq $(cnt) | xargs; echo $(names); echo $(types)) | column -t
 $~: phony; @$($@)
 endif
 
 # $(self) cols
 # 1     2    3      4      5       6     7      8      9      10     11     12   13   14    15
 # meta  dev  mount  inode  nlinks  mode  atime  mtine  ctime  uname  gname  uid  gid  size  path
+# dec   hex  str    dec    dec     hex   time   time   time   str    str    dec  dec  dec   path
 
 ~ := %.stat
 $~: locate = locate -e0d $< /
-$~: fmt := 1 0x%D %m %i %h 0x%f %X %Y %Z %U %G %u %g %s $(pathsep)%n\0
+$~: fmt := 1 0x%D %m %i %h 0x%f %X %Y %Z %U %G %u %g %s $(path.sep)%n\0
 $~: stat = stat --printf='$(fmt)'
 $~: cmd = $(locate) | xargs -r0 $(stat)
 $~: %.db $(self); $(cmd) > $@
 
+assert = assert($1, "$1")
 define awk
-BEGIN {
- RS = "\0";
- ncol = split("$(columns)", col2name);
- for (i = 1; i <= ncol; ++i) name2col[col2name[i]] = i;
-}
-function path(  r, s, q, a) {
- r = "[^:]*$(pathsep)(.*)";
- s = "\\1";
- q = "\"";
- a = "\\\\";
- n = "\n";
- return q gensub(n, a "n", "g", gensub(q, a q, "g", gensub(r, s, 1))) q
-}
-function val(n) { return €name2col[n] }
+function assert(condition, string)
 {
- l = "[";
- c = ",";
- r = "]";
- print l val("uname") c val("gname") c val("size") c path() r
+  if (! condition) {
+    printf("%s:%d: assertion failed: %s\n", FILENAME, FNR, string) > "/dev/stderr";
+    _assert_exit = 1;
+    exit 1
+  }
+}
+function join(a, s) {
+  print length(a);
+  for (i = 1; i < length(a); ++i) print a[i]
+#r = r s a[i];
+#  return r
+}
+BEGIN {
+  RS = "\0";
+  nname = split("$(columns.name)", col2name);
+  ntype = split("$(columns.type)", col2type);
+  $(call assert,nname == ntype);
+  for (i = 1; i <= nname; ++i) name2col[col2name[i]] = i;
+  nuse = split("$(columns.use)", col2use);
+  $(call assert,nuse <= ntype);
+  quote = "\"";
+}
+function val(n) { return €n }
+function dec(n) { return val(n) }
+function time(n) { return val(n) }
+function str(n) { return quote val(n) quote }
+function path(  r, s, q, a) {
+  r = "[^:]*$(path.sep)(.*)";
+  s = "\\1";
+  q = "\"";
+  a = "\\\\";
+  n = "\n";
+  return q gensub(n, a "n", "g", gensub(q, a q, "g", gensub(r, s, 1))) q
+}
+{
+  for (i in col2use) {
+    name = col2use[i];
+    col = name2col[name];
+    type = col2type[col];
+#    print name, col, type;
+    json[name] = @type(col);
+#    print json[name];
+  }
+  join(json, ",")
+#  for (i in json) print json[i]
+}
+0 {
+  l = "[";
+  c = ",";
+  r = "]";
+  print l val("uname") c val("gname") c val("size") c path() r
+}
+END {
+  if (_assert_exit) exit 1
 }
 endef
 
 ~ := %.awk
-$~: %.stat $(self); < $< awk '$(strip $(call €, $(awk)))' > $@
+#$~: %.stat $(self); < $< awk '$(strip $(call €, $(awk)))' > $@
+$~: %.stat $(self); < $< awk '$(call €, $(awk))'
 
 stat: tmp/tst.stat
 awk: tmp/tst.awk
